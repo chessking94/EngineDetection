3969f94348b21b14f57788df590b0a64cdf86d6f
Ethan Hunt <chessking94@gmail.com>
2022-10-15 14:23:45 -0500

    Remove argparse stuff and convert to fully config file

--- src/report/build_report.py
+++ src/report/build_report.py
@@ -1,4 +1,3 @@
-# import argparse
 import logging
 import os
 
@@ -11,41 +10,16 @@ RPT_CHOICES = ['Event', 'Player']  # , 'Game']
 
 # TODO: Scaled ACPL values a la Regan? Maybe less important since I shifted the evaluation weight function so it's not symmetric about 0
 # TODO: More complete ROI that includes more than just the score value
-# TODO: Create slightly different reports for individual games and custom player datasets
-# TODO: Add CLI argument ability
 # TODO: Devise way to pass a PGN file and output the results
 # TODO: Generalize the queries so can pull data from whatever tables I want
-# TODO: May want to revisit hard-coding ROI >= 70 as the determinating factoring an abnormal performance, consider using something more scientific
+# TODO: Asterisks next to EVM, ACPL, SDCPL, and possibly score?
 
 
 def main():
     logging.basicConfig(
-        format='%(funcName)s\t%(levelname)s\t%(message)s',
+        format='%(asctime)s\t%(funcName)s\t%(levelname)s\t%(message)s',
         level=logging.INFO
     )
-
-    # vrs_num = '1.0'
-    # parser = argparse.ArgumentParser(
-    #     description='Evalation Distribution Calculator',
-    #     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    #     usage=argparse.SUPPRESS
-    # )
-    # parser.add_argument(
-    #     '-v', '--version',
-    #     action='version',
-    #     version='%(prog)s ' + vrs_num
-    # )
-    # parser.add_argument(
-    #     '-r', '--report',
-    #     default='Event',
-    #     choices=RPT_CHOICES,
-    #     help='Report Type'
-    # )
-
-    # args = parser.parse_args()
-    # config = vars(args)
-    # rpt = config['report']
-    # logging.debug(f'Arguments|{config}')
 
     config_path = os.path.dirname(__file__)
     for _ in range(2):
@@ -56,8 +30,7 @@ def main():
     db = get_config(config_path, 'useDatabase')
     if db:
         pgn_name = None
-        # engine_name = 'N/A'
-        # depth = 'N/A'
+        # TODO: Get engine and depth values from DB somewhere?
         engine_name = get_config(config_path, 'engineName')
         depth = get_config(config_path, 'depth')
     else:
@@ -65,17 +38,20 @@ def main():
         engine_name = get_config(config_path, 'engineName')
         depth = get_config(config_path, 'depth')
 
-    ev = '81st Tata Steel GpA'
-    pl = ['Magnus', 'Carlsen']  # first, last
-    sdate = '01/01/2019'
-    edate = '12/31/2019'
+    if rpt == 'Event':
+        ev = get_config(config_path, 'eventName')
+    elif rpt == 'Player':
+        first_name = get_config(config_path, 'firstName')
+        last_name = get_config(config_path, 'lastName')
+        full_name = [first_name, last_name]
+        start_date = get_config(config_path, 'startDate')
+        end_date = get_config(config_path, 'endDate')
+
     report_path = get_config(config_path, 'reportPath')
     if rpt == 'Event':
         report_name = f'{ev}_report.txt'
     elif rpt == 'Player':
-        report_name = f'{pl[1]}{pl[0]}_report.txt'
-    # elif rpt == 'Game':
-    #     pass
+        report_name = f'{last_name}{first_name}_report.txt'
     else:
         logging.critical(f'Invalid report type|{rpt}')
         raise SystemExit
@@ -87,7 +63,7 @@ def main():
 
     # headers
     with open(report_full, 'w') as rf:
-        sections.header_type(rf, rpt, conn, ev, pl, sdate, edate)
+        sections.header_type(rf, rpt, conn, ev, full_name, start_date, end_date)
         sections.header_info(rf, pgn_name, engine_name, depth)
         sections.scoring_desc(rf, conn)
 
@@ -98,13 +74,11 @@ def main():
             sections.game_key(rf)
             sections.game_traces(rf, conn, ev, None, None, None)
         elif rpt == 'Player':
-            sections.key_stats(rf, conn, None, pl, sdate, edate)
+            sections.key_stats(rf, conn, None, full_name, start_date, end_date)
             sections.player_key(rf)
-            sections.player_summary(rf, conn, None, pl, sdate, edate)
+            sections.player_summary(rf, conn, None, full_name, start_date, end_date)
             sections.game_key(rf)
-            sections.game_traces(rf, conn, None, pl, sdate, edate)
-        # elif rpt == 'Game':
-        #     pass
+            sections.game_traces(rf, conn, None, full_name, start_date, end_date)
 
     conn.close()
 
